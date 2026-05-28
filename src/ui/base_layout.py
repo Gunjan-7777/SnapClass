4425ffcacd96a21f9c83bebebf1ad43af2bb0365
Gunjan-7777 <gunjansidana77@gmail.com>
2026-05-28 16:07:38 +0530

    snap class

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -68,7 +68,7 @@ def style_base_layout():
 
                 button{
                     border-radius: 1.5rem !important;
-                    background: #5685F2 !important;
+                    background-color: #5685F2 !important;
                     color: white !important;
                     padding: 10px 20px !important;
                     border: none !important;
@@ -77,7 +77,7 @@ def style_base_layout():
 
                 button[kind='secondary']{
                     border-radius: 1.5rem !important;
-                    background: #EB459E !important;
+                    background-color: #EB459E !important;
                     color: white !important;
                     padding: 10px 20px !important;
                     border: none !important;
@@ -86,7 +86,7 @@ def style_base_layout():
 
                 button[kind='tertiary']{
                     border-radius: 1.5rem !important;
-                    background: black !important;
+                    background-color: black !important;
                     color: white !important;
                     padding: 10px 20px !important;
                     border: none !important;
